a08be154bbe37f38c22c672c175263722e3947de
Rodrigo <rods.dduarte@gmail.com>
2024-04-01 22:02:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,3 @@
+function skillsMember() {
+  return 'I am a member of the team and I have the following skills: HTML, CSS, JavaScript, React, Node.js, MongoDB, SQL, Python, Java, C++, C#, Ruby, PHP, etc.'
+}
